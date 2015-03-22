2ea45a721392e8b4d03a9e402c14c1e9b77aed9c
TheNotary <no@mail.plz>
2015-03-21 21:11:07 -0500

    Prototype complete

--- lib/bundlegem.rb
+++ lib/bundlegem.rb
@@ -5,4 +5,20 @@ require "bundlegem/version"
 
 module Bundlegem
   # Your code goes here...
+  
+  class << self
+    def which(executable)
+      if File.file?(executable) && File.executable?(executable)
+        executable
+      elsif ENV['PATH']
+        path = ENV['PATH'].split(File::PATH_SEPARATOR).find do |p|
+          abs_path = File.join(p, executable)
+          File.file?(abs_path) && File.executable?(abs_path)
+        end
+        path && File.expand_path(executable, path)
+      end
+    end
+    
+  end
+  
 end
