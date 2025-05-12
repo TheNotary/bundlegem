47c222123e36033f0d089b86288038c70d8e6cfa
TheNotary <no@email.plz>
2025-05-11 19:23:49 -0500

    cleans up help text

--- lib/bundlegem/strings.rb
+++ lib/bundlegem/strings.rb
@@ -5,17 +5,14 @@ Use bundlegem to start a new project folder based on a predefined template.
 
 Usage Examples:
 
-  # Make a new ruby gem
-  $  bundlegem your_gem_name
+  # Download all my template files (configured in ~/.bundlegem/config)
+  $ bundlegem --install-best-templates
 
   # List available teplates
   $  bundlegem --list
 
   # Create a ruby gem project using the built in service template
-  $ bundlegem your_gem_name -t service
-
-  # Download all my template files (configured in ~/.bundlegem/config)
-  $ bundlegem --install-best-templates
+  $ bundlegem --template service your_gem_name
 
   # Convert the current directory which represents a working project into a
   # template meaning all files will be renamed to *.tt unless a *.tt of that
