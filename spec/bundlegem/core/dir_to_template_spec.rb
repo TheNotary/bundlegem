0bdb390c95e670ee7e6a452ea2803fe93a258d65
TheNotary <no@email.plz>
2025-06-16 19:10:13 -0500

    finishes writing dir_to_template_spec

--- spec/bundlegem/core/dir_to_template_spec.rb
+++ spec/bundlegem/core/dir_to_template_spec.rb
@@ -10,13 +10,31 @@ module Bundlegem::Core
 
       reset_test_env
       FileUtils.chdir(@dst_dir)
+      @initial_dir = FileUtils.pwd
     end
 
-    it 'is there' do
-      create_user_defined_template(category: "wizardly_tools")
-      files_changed = DirToTemplate.🧙🪄! Find.find('/tmp/temp/.'), dry_run: true
+    after :each do
+      FileUtils.cd(@initial_dir)
+    end
 
-      expect(files_changed.first).to eq "Renamed: /tmp/temp/./README.md -> /tmp/temp/./README.md.tt"
+    it 'will rename the expected files and ignore the ones that should be ignored' do
+      template_dir = create_user_defined_template(category: "wizardly_tools")
+
+      gitignored_file = "something.toignore"
+
+      # Setup test template
+      FileUtils.cd(template_dir)
+      FileUtils.touch("#{template_dir}/README.md")
+      FileUtils.touch("#{template_dir}/#{gitignored_file}")
+      File.write("#{template_dir}/.gitignore", gitignored_file)
+      `git init`
+
+      files_changed = DirToTemplate.🧙🪄! Find.find("."), dry_run: true
+
+      expect(files_changed[0]).to eq "Renamed: ./.gitignore -> ./.gitignore.tt"
+      expect(files_changed[1]).to eq "Renamed: ./README.md -> ./README.md.tt"
+      expect(File).to exist "#{template_dir}/.gitignore"
+      expect(files_changed.count).to eq 2
     end
   end
 end
